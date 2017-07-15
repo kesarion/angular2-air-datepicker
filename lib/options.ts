0387799088a40bc36e309646e5ca90c035548f2c
Alexandru Sfirlogea <alexandru.sfirlogea@gmail.com>
2017-07-15 22:57:00 +0300

    implemented enabledDateRanges option; improvements and bug fixes
    Closes #4

--- lib/options.ts
+++ lib/options.ts
@@ -5,13 +5,31 @@ export class AirOptions {
     language: string;
     hourStep: number;
     minuteStep: number;
+    enabledDateRanges: DateRange[];
 
-    constructor () {
-        this.timepicker = false;
-        this.format12h = false;
-        this.fullDays = false;
-        this.language = 'en';
-        this.hourStep = 1;
-        this.minuteStep = 1;
+    constructor (options: AirOptions = {} as AirOptions) {
+        this.timepicker = !!options.timepicker;
+        this.format12h = !!options.format12h;
+        this.fullDays = !!options.fullDays;
+        this.language = options.language || 'en';
+        this.hourStep = options.hourStep || 1;
+        this.minuteStep = options.minuteStep || 1;
+        this.enabledDateRanges = options.enabledDateRanges || [];
     }
+
+    isDisabled (date: Date) {
+      for (let dateRange of this.enabledDateRanges) {
+        // console.log(date, dateRange, date >= dateRange.start, date <= dateRange.end, date >= dateRange.start && date <= dateRange.end);
+        if (date >= dateRange.start && date <= dateRange.end) {
+          return false;
+        }
+      }
+
+      return !!this.enabledDateRanges.length;
+    }
+}
+
+interface DateRange {
+  start: Date;
+  end: Date;
 }
